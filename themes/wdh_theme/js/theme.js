691f96398a3144140b5ec41e3ba00452ac827177
tikaszvince <vince.tikasz@gmail.com>
2013-10-19 13:29:30 +0200

    import basic theme.js contains Drupal behavior

---
+++ themes/wdh_theme/js/theme.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Theme behavior.
+ */
+
+/*global jQuery */
+/*global Drupal */
+
+;(function($, window, document, undefined){
+  "use strict";
+
+  Drupal.behaviors.wdh_theme = {
+    attach: function(context, settings) {
+
+    }
+  };
+
+}(jQuery, window, document, undefined));
